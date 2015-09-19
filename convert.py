62abf5afbfe40e0e03fee3a4418d5eb85b9d55a3
mnaka <motoki.nakamura@mail.mcgill.ca>
2015-09-19 16:22:18 -0400

    prints most relevant information

--- convert.py
+++ convert.py
@@ -1,16 +1,23 @@
 import sys
 
-x = input("Enter your keyword: ")
+#x = input("Enter your keyword: ")
+keyword = input("Enter your keyword: ")
 def search (keyword):
     for x in range (1,len(sys.argv)):
-        print sys.argv[x]
         f = open(sys.argv[x], 'r')
-        text = f.read().lower()
-        if keyword.lower() in text:
+        text = f.read().lower().split()
+        appear = False
+        counter = 0
+        where_appear = []
+        for i in range(0, len(text)):
+            if text[i] == keyword.lower():
+
+                counter = counter + 1
+                where_appear.append(i)
+                appear = True
+        if appear == True:
             print sys.argv[x]
             print keyword
-            
-            return keyword
-        else:
-            return False
-search(x)
+            print counter
+            print where_appear
+search(keyword)
