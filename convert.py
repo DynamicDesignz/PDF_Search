81b0ec46974d666fa50f692e566127293306bd56
mnaka <motoki.nakamura@mail.mcgill.ca>
2015-09-19 16:52:55 -0400

    working on dat summary yo

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import sys
 
-#x = input("Enter your keyword: ")
-keyword = input("Enter your keyword: ")
+#keyword = input("Enter your keyword: ")
+keyword = "have"
 def search (keyword):
     for x in range (1,len(sys.argv)):
         f = open(sys.argv[x], 'r')
@@ -11,13 +11,25 @@ def search (keyword):
         where_appear = []
         for i in range(0, len(text)):
             if text[i] == keyword.lower():
-
                 counter = counter + 1
                 where_appear.append(i)
                 appear = True
         if appear == True:
-            print sys.argv[x]
-            print keyword
-            print counter
-            print where_appear
+            out = open('output.txt', 'w')
+            out.write(sys.argv[x])
+            out.write("\n")
+            out.write(keyword)
+            out.write("\n")
+            out.write(str(counter))
+            out.write("\n")
+            out.write("\n")
+            for i in range(0,len(where_appear)):
+                for j in range (-25,25):
+                    if text[where_appear[i] + j] == keyword:
+                        out.write(keyword.upper())
+                    else:
+                        out.write(text[where_appear[i] + j] ,)
+                out.write("\n")
+
+
 search(keyword)
