487a9ff45f69aed14e056212685f6f58205293f4
mnaka <motoki.nakamura@mail.mcgill.ca>
2015-09-19 18:49:52 -0400

    multiple keywords work

--- convert.py
+++ convert.py
@@ -1,36 +1,44 @@
 import sys
 
 #keyword = input("Enter your keyword: ")
-keyword = "have"
-def search (keyword):
-    for x in range (1,len(sys.argv)):
-        f = open(sys.argv[x], 'r')
-        text = f.read().lower().split()
-        appear = False
-        counter = 0
-        where_appear = []
-        for i in range(0, len(text)):
-            if text[i] == keyword.lower():
-                counter = counter + 1
-                where_appear.append(i)
-                appear = True
-        if appear == True:
-            out = open('output.txt', 'w')
-            out.write(sys.argv[x])
-            out.write("\n")
-            out.write(keyword)
-            out.write("\n")
-            out.write(str(counter))
-            out.write("\n")
-            out.write("\n")
-            for i in range(0,len(where_appear)):
-                for j in range (-25,25):
-                    if text[where_appear[i] + j] == keyword:
-                        out.write(keyword.upper() + " ")
-                    else:
-                        out.write(text[where_appear[i] + j] + " ",)
-                out.write("\n")
-                out.write("\n")
 
+keyword = ["what", "are", "you", "talking", "about", "have"]
+def search (keyword):
+    appear = False
+
+    where_appear = []
+    out = open('output.txt', 'w')
+
+    #Search through every inputted pdf
+    for pdf_loop in range (1,len(sys.argv)):
+        f = open(sys.argv[pdf_loop], 'r')
+        text = f.read().lower().split()
+
+        #Search through every inputted keyword
+        for keyword_loop in range (0, len(keyword)):
+            counter = 0
+
+            for i in range(0, len(text)):
+                if text[i] == keyword[keyword_loop].lower():
+                    counter = counter + 1
+                    where_appear.append(i)
+
+            out.write(sys.argv[pdf_loop] + "\n")
+            out.write(keyword[keyword_loop] + "\n")
+            out.write(str(counter) + "\n")
+            for i in range(0,len(where_appear)):
+                if where_appear[i] < 25:
+                    for j in range (0,50):
+                        out.write(text[where_appear[i] + j] + " ",)
+                elif where_appear[i] > len(text)-25:
+                    for j in range (len(text) - 50, len(text)):
+                        out.write(text[j] + " ",)
+                else:
+                    for j in range (-25,25):
+                        if text[where_appear[i] + j] == keyword[keyword_loop]:
+                            out.write(keyword[keyword_loop].upper() + " ")
+                        else:
+                            out.write(text[where_appear[i] + j] + " ",)
+            out.write("\n" + "\n")
 
 search(keyword)
